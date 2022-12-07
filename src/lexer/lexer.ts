e1fbfb21faf09576919040002e9204ef68ff61c1
Caio Oliveira <caio.goncalves.pro@gmail.com>
2022-12-07 02:58:56 -0300

    Testing a different structure

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -1,1 +1,132 @@
 // This is the API to the automata so that the syntax analyzer can use it
+import { Token, TypeofToken } from './lexer.types'
+
+export function tokeniser(input: string): Token[] {
+  function lookaheadString(str: string): boolean {
+    const parts = str.split('')
+
+    for (let i = 0; i < parts.length; i++) {
+      if (input[currentPosition + i] !== parts[i]) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  function lookahead(match: RegExp, matchNext?: RegExp): string[] {
+    const bucket: string[] = []
+
+    while (true) {
+      const nextIndex = currentPosition + bucket.length
+      const nextToken = input[nextIndex]
+      if (!nextToken) {
+        break
+      }
+      let m: string | RegExp = match
+      if (matchNext && bucket.length) {
+        m = matchNext
+      }
+      if (m && !m.test(nextToken)) {
+        break
+      }
+      bucket.push(nextToken)
+    }
+
+    return bucket
+  }
+
+  const out: Token[] = []
+  let currentPosition = 0
+
+  const tokenStringMap: Array<{
+    key: string
+    value: Token
+  }> = [
+    { key: '\n', value: { type: TypeofToken.IGNORAR } },
+    { key: 'inicio', value: { type: TypeofToken.inicio } },
+    { key: '\n', value: { type: TypeofToken.IGNORAR } },
+    { key: 'varinicio', value: { type: TypeofToken.varinicio } },
+    { key: '\n', value: { type: TypeofToken.IGNORAR } },
+    { key: 'inteiro', value: { type: TypeofToken.inteiro } },
+    { key: ';', value: { type: TypeofToken.PT_V } },
+    { key: '\n', value: { type: TypeofToken.IGNORAR } },
+    { key: 'varfim', value: { type: TypeofToken.varfim } },
+    { key: ';', value: { type: TypeofToken.PT_V } },
+    { key: '\n', value: { type: TypeofToken.IGNORAR } },
+    { key: '<-', value: { type: TypeofToken.OPR } },
+    { key: ';', value: { type: TypeofToken.PT_V } },
+    { key: '\n', value: { type: TypeofToken.IGNORAR } },
+    { key: 'escreva', value: { type: TypeofToken.escreva } },
+    { key: ';', value: { type: TypeofToken.PT_V } },
+    { key: 'fim', value: { type: TypeofToken.fim } }
+  ]
+
+  while (currentPosition < input.length) {
+    const currentToken = input[currentPosition]
+
+    if (currentToken === ' ') {
+      currentPosition++
+      continue
+    }
+
+    let didMatch = false
+
+    for (const { key, value } of tokenStringMap) {
+      if (!lookaheadString(key)) {
+        continue
+      }
+
+      out.push(value)
+      currentPosition += key.length
+      didMatch = true
+    }
+
+    if (didMatch) continue
+
+    if (currentToken === "'") {
+      currentPosition++
+
+      const bucket = lookahead(/[^']/)
+
+      out.push({
+        type: TypeofToken.ConstanteLiteral,
+        value: bucket.join('')
+      })
+
+      currentPosition += bucket.length + 1
+
+      continue
+    }
+
+    const literalRegex = /[a-zA-Z]/
+    const literalRegexNext = /[a-zA-Z0-9]/
+
+    if (literalRegex.test(currentToken)) {
+      const bucket = lookahead(literalRegex, literalRegexNext)
+
+      out.push({
+        type: TypeofToken.Identificador,
+        value: bucket.join('')
+      })
+
+      currentPosition += bucket.length
+
+      continue
+    }
+    throw new Error(`Unknown input character: ${currentToken}`)
+  }
+
+  console.log(
+    tokeniser(`
+  inicio
+  varinicio
+  inteiro A;
+  varfim;
+  escreva A;
+  fim
+  `)
+  )
+
+  return out
+}
--- src/lexer/lexer.types.ts
+++ src/lexer/lexer.types.ts
@@ -1,12 +1,72 @@
 import { Reader } from '@/lexer/file.types'
-import { TypeofToken } from './automata.types'
+//import { TypeofToken } from './automata.types'
 
-type Token = {
-  type: TypeofToken
-  details: string
-  start: number
-  end: number
+enum TypeofToken {
+  ConstanteNumerica = 'NUM',
+  ConstanteLiteral = 'LIT',
+  Identificador = 'ID',
+  Comentario = 'COMMENT',
+  EndOfFile = 'EOF',
+  OPR = 'OPR',
+  ATR = 'ATR',
+  OPA = 'OPA',
+  AB_P = 'AB_P',
+  FC_P = 'FC_P',
+  PT_V = 'PT_V',
+  VIR = 'VIR',
+  ERROR = 'ERROR',
+  IGNORAR = 'IGNORAR',
+  inicio = 'inicio',
+  varinicio = 'varinicio',
+  varfim = 'varfim',
+  escreva = 'escreva',
+  leia = 'leia',
+  se = 'se',
+  entao = 'entao',
+  fimse = 'fimse',
+  fim = 'fim',
+  inteiro = 'inteiro',
+  literal = 'literal',
+  real = 'real'
 }
+interface TokenNode<T extends TypeofToken> {
+  type: T
+}
+
+interface TokenValueNode<T extends TypeofToken> extends TokenNode<T> {
+  value: string
+}
+
+type Token =
+  | TokenNode<TypeofToken.Comentario>
+  | TokenNode<TypeofToken.EndOfFile>
+  | TokenNode<TypeofToken.OPR>
+  | TokenNode<TypeofToken.ATR>
+  | TokenNode<TypeofToken.OPA>
+  | TokenNode<TypeofToken.AB_P>
+  | TokenNode<TypeofToken.FC_P>
+  | TokenNode<TypeofToken.PT_V>
+  | TokenNode<TypeofToken.VIR>
+  | TokenNode<TypeofToken.ERROR>
+  | TokenNode<TypeofToken.IGNORAR>
+  | TokenNode<TypeofToken.inicio>
+  | TokenNode<TypeofToken.varinicio>
+  | TokenNode<TypeofToken.varfim>
+  | TokenNode<TypeofToken.escreva>
+  | TokenNode<TypeofToken.leia>
+  | TokenNode<TypeofToken.se>
+  | TokenNode<TypeofToken.entao>
+  | TokenNode<TypeofToken.fimse>
+  | TokenNode<TypeofToken.fim>
+  | TokenNode<TypeofToken.inteiro>
+  | TokenNode<TypeofToken.literal>
+  | TokenNode<TypeofToken.real>
+  | TokenValueNode<TypeofToken.ConstanteNumerica>
+  | TokenValueNode<TypeofToken.Identificador>
+  | TokenValueNode<TypeofToken.ConstanteLiteral>
+/* details: string
+  start: number
+  end: number */
 
 type Lexer = {
   nextToken(): Token
@@ -14,4 +74,4 @@ type Lexer = {
 
 type LexerConstructor = (reader: Reader) => Lexer
 
-export { Lexer, LexerConstructor }
+export { Lexer, LexerConstructor, Token, TypeofToken }
