f4cfe97f8294932a74698f6a7f4be9e39d0de29f
Tarek Campos <tarekcampossaleh@gmail.com>
2022-12-11 14:27:11 -0300

    feat: add file reader

--- src/lexer/file.ts
+++ src/lexer/file.ts
@@ -1,1 +1,40 @@
-// This will read the file(s) and return an object that the automata can use
+import path from 'path'
+import { readFileSync } from 'fs'
+import { Reader, ReaderConstructor } from './file.types'
+
+const readFile = (path: string) => readFileSync(path, 'utf-8').split('')
+
+const Reader: ReaderConstructor = (pathString: string) => {
+  if (path.extname(pathString) != '.mgol') {
+    throw new Error(
+      `Arquivo de extensão não reconhecido: ${path.extname(pathString)}`
+    )
+  }
+  const file = readFile(pathString)
+  let iterator = -1
+  let line = 1
+  let column = 0
+
+  const nextChar = () => {
+    if (iterator + 1 === file.length)
+      return { char: 'EOF', charNumber: iterator, line: line, column: column }
+
+    iterator++
+    column++
+    if (file[iterator - 1] === '\n') {
+      line++
+      column = 0
+    }
+
+    return {
+      char: file[iterator],
+      charNumber: iterator,
+      line: line,
+      column: column
+    }
+  }
+
+  return { nextChar: nextChar }
+}
+
+export { Reader }
